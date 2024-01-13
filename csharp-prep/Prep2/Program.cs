7d354f6c5665d85054c5d3ddc6ff461604d66fc5
elabanca <elabanca@byui.edu>
2024-01-12 19:18:36 -0700

    1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter Number: ");
+        string user_entry= Console.ReadLine();
+
+        int x = int.Parse (user_entry);
+        
+        if (x >= 90)
+        {
+            Console.Write("A");
+        }
+        else if (x>=80)
+        {
+            Console.Write("B");
+        }
+        else if(x>= 70)
+        {
+            Console.Write("C");
+        }
+        else if (x >= 60)
+        {
+            Console.Write("D");
+        }
+        else if (x < 60 )
+        {
+            Console.Write("F");
+        }
     }
 }
