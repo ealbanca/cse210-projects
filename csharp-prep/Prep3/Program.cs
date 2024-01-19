b454cd6fd61d3e92de85e38a4d7c1c6bf6f2251e
elabanca <elabanca@byui.edu>
2024-01-18 19:39:58 -0700

    i

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        int guess_number = -1;
+        
+
+        while ( guess_number != magic_number)
+        {
+            Console.WriteLine("what is your guest? ");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (guess_number > magic_number)
+            {
+            Console.WriteLine("lower ");
+            }
+            else if (guess_number< magic_number)
+            {
+            Console.WriteLine("higher");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+        }
+
+
+
     }
 }
