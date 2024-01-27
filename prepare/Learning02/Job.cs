f9240518e92be4443f4c119559a08b258ffdade7
elabanca <elabanca@byui.edu>
2024-01-26 18:46:29 -0700

    1

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -9,7 +9,7 @@ public class Job
 
     public void Display()
     { 
-        Console.WriteLine($"{_company}");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
     
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,13 +9,22 @@ class Program
         job1._company = "Google";
         job1._startYear = 2010;
         job1._endYear = 2015;
+        job1.Display();
 
         Job job2 = new Job();
-        job2._jobTitle = "Software Engineer";
+        job2._jobTitle = "Team Manager";
         job2._company = "Facebook";
-        job2._startYear = 2015;
+        job2._startYear = 2016;
         job2._endYear = 2023;
+        job2.Display();
 
+        Resume myResume = new Resume();
+        myResume._name = "Hared Albancando Robles";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+        
     }
 
 }
